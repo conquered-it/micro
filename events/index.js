eb3b4b78c521e778cc036a7d7c530a126f55fbbc
conquered-it <gps.vsgg@gmail.com>
2021-03-08 17:52:24 +0530

    v1.0.2

--- events/index.js
+++ events/index.js
@@ -7,6 +7,7 @@ cors = require('cors'),
 qs = require('qs'),
 port = process.env.PORT || 4005;
 mongoose.set('useNewUrlParser', true);
+var MongoClient = require('mongodb').MongoClient;
 mongoose.connect("mongodb://localhost/events",{ useUnifiedTopology: true });
 app.use(bodyParser.urlencoded({extended:true}));
 app.set("view engine","ejs");
@@ -17,28 +18,60 @@ app.use(cors());
 // 'updatedcomment' -> query
 // 'moderatedcomment' -> comments
 
+var createpostSchema = new mongoose.Schema({
+    post:{
+        title:String,
+        body:String
+    }
+});
+
+var createpost = mongoose.model("createpost",createpostSchema);
+
 var data={
     'newpost':['http://localhost:4002/events'],
     'newcomment':['http://localhost:4002/events','http://localhost:4003/events'],
     'updatedcomment':['http://localhost:4002/events'],
-    'moderatedcomment':['http://localhost:4001/events']
+    'moderatedcomment':['http://localhost:4001/events'],
+    'createpost':['http://localhost:4000/posts']
 };
 
+var store={
+    'createpost':createpost
+}
+
+var update={
+    'createpost':'http://localhost:4000/posts',
+}
 
 app.post('/events',function(req,res){
     var event = req.body;
     data[event.type].forEach(async function(x){
-        await axios.post(x,qs.stringify(event)).catch(function(err){
-            store[event.type].create(event);
-        });
+        try{
+            await axios.post(x,qs.stringify(event))
+        }catch{
+            console.log("This service is currently not available, your changes will be soon updated");
+            store[event.type].create({post:event.post},function(err,ret){
+                if(err) console.log('err');
+            });
+        };
     })
-    // console.log(event);
-    // axios.post('http://localhost:4000/events',qs.stringify(event));
-    // axios.post('http://localhost:4001/events',qs.stringify(event));
-    // axios.post('http://localhost:4002/events',qs.stringify(event));
-    // axios.post('http://localhost:4003/events',qs.stringify(event));
-    res.send({});
+    res.redirect('http://localhost:3000');
 });
+
+app.post('/check',async function(req,res){
+    var event = req.body;
+    store[event.type].find({},async function(err,ret){
+        ret.forEach(async function(x){
+            await axios.post(update[event.type],qs.stringify(x));
+        })
+        func(store[event.type]);
+    })
+    function func(val){
+        val.remove({},function(err){
+            if(err) console.log('err');
+        });
+    }
+})
 
 app.listen(port,function(){
     console.log('ok_events');
